39e80cee6014b1399b67627fca9b9c0c02ad967a
rspothu <43735416+rspothu@users.noreply.github.com>
2025-08-16 06:20:31 +0900

    :sparkles: setup eslint / lint-staged (#48)
    * :sparkles: setup eslint / lint-staged
    
    * :sparkles: add lint workflow
    
    * :sparkles: updated lint workflow
    
    * :sparkles: updated for pnpm store files ignore

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import antfu from "@antfu/eslint-config";
+
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(antfu({
+  type: "app",
+  vue: true,
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+  ignores: ["./src/github/schema.docs.graphql", ".pnpm-store/**"],
+}, {
+  rules: {
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": ["off"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
+}));
