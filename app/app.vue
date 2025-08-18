e82391d1516a5ee0c80887947a193424ac5eac23
rspothu <43735416+rspothu@users.noreply.github.com>
2025-08-18 13:06:47 +0900

    Update app.vue
    Removed text & button outside of hero page

--- app/app.vue
+++ app/app.vue
@@ -2,12 +2,6 @@
   <div>
     <NuxtLayout>
       <NuxtPage />
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <button class="btn btn-secondary">
-        View Map <Icon name="uil:github" style="color: black" />
-      </button>
     </NuxtLayout>
   </div>
 </template>
