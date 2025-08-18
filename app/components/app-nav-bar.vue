ef16fad21784f216354dab4961e16673764cc620
rspothu <43735416+rspothu@users.noreply.github.com>
2025-08-18 13:08:35 +0900

    Tl 11 (#50)

---
+++ app/components/app-nav-bar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { NuxtLink } from "#components";
+</script>
+
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Sign In with GitHub
+        <Icon name="tabler:brand-github" class="24" />
+      </button>
+    </div>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventures. Add locations,  photos, and notes to create a personal digital journal of your journeys.
+        </p>
+        <button class="btn btn-accent">
+          Sign In with GitHub
+          <Icon name="tabler:brand-github" class="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
