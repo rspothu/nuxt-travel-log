1bd39c0d7896ae13dd152295a5d6bd9d4b6abda5
rspothu <rspothu@gmail.com>
2025-08-18 14:01:39 +0900

    :sparkles: add theme toggle

--- app/components/app-nav-bar.vue
+++ app/components/app-nav-bar.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { NuxtLink } from "#components";
+
+import AppThemeToggle from "./app-theme-toggle.vue";
 </script>
 
 <template>
@@ -10,6 +12,8 @@ import { NuxtLink } from "#components";
       </NuxtLink>
     </div>
     <div class="navbar-end">
+      <AppThemeToggle />
+
       <button class="btn btn-accent">
         Sign In with GitHub
         <Icon name="tabler:brand-github" class="24" />
---
+++ app/components/app-theme-toggle.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value: boolean) {
+    colorMode.value = value ? "dark" : "light";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate mx-4">
+    <input v-model="isDark" type="checkbox">
+    <Icon class="swap-on" name="tabler:sun" size="24" />
+    <Icon class="swap-off" name="tabler:moon" size="24" />
+  </label>
+</template>
