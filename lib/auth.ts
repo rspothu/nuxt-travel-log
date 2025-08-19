60057ece01c796e7e5d14ac4a6b0b36e3f0d58b2
rspothu <rspothu@gmail.com>
2025-08-19 17:47:13 +0900

    :sparkles: add trusted origin for vercel

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,7 +8,7 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
-  trustedOrigins: ["http://localhost:3000/", "http://localhost:3000/dashboard/"],
+  trustedOrigins: ["http://localhost:3000/", "http://localhost:3000/dashboard/", "http://localhost:3000/error/", "https://nuxt-travel-log-git-tl-16-rspothus-projects.vercel.app/", "https://nuxt-travel-log-git-tl-16-rspothus-projects.vercel.app/dashboard/", "https://nuxt-travel-log-git-tl-16-rspothus-projects.vercel.app/error/"],
   advanced: {
     database: {
       generateId: false,
